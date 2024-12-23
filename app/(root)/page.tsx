1ae501a1657ec67135ef73d6d80625c3626c06b4
Ishan Gautam <ishan0gautam0@gmail.com>
2024-12-23 19:29:28 +0545

    search form integration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,25 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "../../components/ui/SearchForm";
+import StartupCard from "../../components/ui/StartupCard";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: "Yesterday",
+      views: 55,
+      author: { _id: 1 },
+      _id: 1,
+      description: "This is a description",
+      category: "robots",
+      title: "We robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -11,7 +30,23 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vouce Your Startup, Grow Your Business
         </p>
-        <SearchForm />
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results For ${query}` : "All Startup"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startup Found</p>
+          )}
+        </ul>
       </section>
     </>
   );
---
+++ components/ui/StartupCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StartupCard = ({ posts: StartupTypeCard }) => {
+  return (
+    <li className="startup-card group">
+      <p className="startup_card_date"></p>
+    </li>
+  );
+};
+
+export default StartupCard;
